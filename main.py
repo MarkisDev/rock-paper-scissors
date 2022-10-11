ac75efff11e965c6083b3294e0a43784c16a7407
Rijuth Menon <rijuthm@gmail.com>
2022-10-11 23:32:11 +0530

    added constructor

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+from github import Github
+
+
+class RPS:
+    def __init__(self, token, issueNumber, repo):
+        self.token = token
+        self.repo = Github(token).get_repo(repo)
+        self.issue = self.repo.get_issue(issueNumber)
+        self.moves = ['rock', 'paper', 'scissor']
+        self.fileName = 'README.md'
